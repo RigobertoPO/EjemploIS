5be4b53a75fc98d208e1174916af31ce8e774337
signsoluciones <reptors58@hotmail.com>
2019-09-05 19:48:42 -0500

    libreria

---
+++ PEPSI/PEPSI.Calculadora.Library/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEPSI.Calculadora.Library
+{
+    public class Class1
+    {
+    }
+}
